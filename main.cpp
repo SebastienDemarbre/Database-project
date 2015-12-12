56397279d038129a2a60d9db5c2abca8a2d9e710
Sebastien Demarbre <seb.demarbre@gmail.com>
2015-12-12 17:26:56 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QCoreApplication>
+#include <QtSql/QSqlDatabase>
+#include <iostream>
+
+#define q2c(string) string.toStdString()
+
+
+int main(int argc, char *argv[])
+{
+
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
